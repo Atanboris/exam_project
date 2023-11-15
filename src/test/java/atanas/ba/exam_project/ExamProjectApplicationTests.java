29f37613b4587ca19702218432c6a2e933cce58c
Atanboris <drakensang909@gmail.com>
2023-11-15 15:39:50 +0100

    INit commit

---
+++ src/test/java/atanas/ba/exam_project/ExamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package atanas.ba.exam_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
