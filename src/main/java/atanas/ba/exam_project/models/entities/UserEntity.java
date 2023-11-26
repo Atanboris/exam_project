727cb17f39cb398bc52bc7d644180af52e58b91d
Atanboris <drakensang909@gmail.com>
2023-11-26 18:00:20 +0100

    NaN

---
+++ src/main/java/atanas/ba/exam_project/models/entities/UserEntity.java
@@ -0,0 +1,8 @@
+package atanas.ba.exam_project.models.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class UserEntity extends BaseEntity {
+
+}
---
+++ src/main/java/atanas/ba/exam_project/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package atanas.ba.exam_project.service.impl;
+
+import atanas.ba.exam_project.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
